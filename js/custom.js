8ec2f9e3fd8c73dfd0a751d5723b719f77707dab
gilbrater <gilbrater1@gmail.com>
2017-05-20 01:24:38 +0100

    CHNG-203

--- js/custom.js
+++ js/custom.js
@@ -3,14 +3,7 @@ SERVICES
 ======================================================*/
 
 $(function () {
-    wow = new WOW({
-        boxClass: 'wow', // default
-        animateClass: 'animated', // default
-        offset: 0, // default
-        mobile: true, // default
-        live: true // default
-    });
-    wow.init();
+    wow = new WOW().init();
 });
 
 
@@ -25,5 +18,72 @@ $(function () {
         gallery: {
             enabled: true
         }
+    });
+});
+
+
+/*=====================================================
+TEAMS
+======================================================*/
+
+$(function () {
+    $('#team-carousel').owlCarousel({
+        loop: true,
+        items: 3,
+        margin: 30,
+        stagePadding: 30,
+        smartSpeed: 450,
+        mouseDrag: true,
+        touchDrag: true,
+        dots: true,
+        dotsEach: true,
+        autoplay: true
+    });
+});
+
+/*=====================================================
+TESTIMONIALS
+======================================================*/
+
+$(function () {
+    $('#testimonial-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        margin: 30,
+        stagePadding: 30,
+        smartSpeed: 450,
+        mouseDrag: true,
+        touchDrag: true,
+        dots: true,
+        dotsEach: true,
+        autoplay: true
+    });
+});
+
+/*=====================================================
+STATS
+======================================================*/
+$(function () {
+    $('.counter').counterUp({
+        delay:30,
+        time:2000
+    });
+});
+
+/*=====================================================
+CLIENTS
+======================================================*/
+$(function () {
+    $('#client-carousel').owlCarousel({
+        loop: true,
+        items: 6,
+        margin: 30,
+        stagePadding: 30,
+        smartSpeed: 450,
+        mouseDrag: true,
+        touchDrag: true,
+        dots: true,
+        dotsEach: true,
+        autoplay: true
     });
 });
